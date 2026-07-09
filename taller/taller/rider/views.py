be63bcea054252383671fce9d2ae7df62363219d
Jeank935 <jccuenca17@utpl.edu.ec>
2026-07-09 11:54:04 -0500

    2

--- taller/taller/rider/views.py
+++ taller/taller/rider/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from .models import Edificio, Departamento
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+# ejemplo de uso django-rest_framework
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from rider.serializers import EdificioSerializer, DepartamentoSerializer
+
+
+from rider.forms import *
 
 
 def index(request):
