89353ddec0ca2a2871d2cc16ae5e47d1d54d7c2e
Jeank935 <jccuenca17@utpl.edu.ec>
2026-07-09 11:22:00 -0500

    v2

--- taller/taller/rider/admin.py
+++ taller/taller/rider/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 
-# class EdificioAdmin(admin.ModelAdmin):
-#     list_display = ('nombre', 'apellido', 'cedula')
-#     search_fields = ('nombre', 'cedula')
 
-# admin.site.register(Estudiante, EstudianteAdmin)
+from rider.models import Edificio, Departamento
+class EdificioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'direccion', 'ciudad','tipo')
+    search_fields = ('nombre', 'direccion')
+
+admin.site.register(Edificio, EdificioAdmin)
+
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = ('nombre_completo_propietario', 'costo_departamento', 'numero_cuartos','edificio')
+    search_fields = ('nombre_completo_propietario', 'numero_cuartos')
+
+admin.site.register(Departamento, DepartamentoAdmin)
